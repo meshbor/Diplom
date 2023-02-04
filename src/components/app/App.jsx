153624775fe98289ebb938a677712eac81dae87c
Evgeny <sipatrov_e@mail.ru>
2023-02-05 00:02:32 +0400

    отрендерил карточки

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,23 +1,37 @@
 
-import React from 'react';
-// import {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
+
 // import logo from './logo.svg';
 import './App.css';
 import CardList from '../cardList/CardList';
-// import data from '../assets/data.json';
-// import { useState } from 'react';
+
+import api from '../utilites/api';
 
 function App() {
 
+    const [cards, setCards]=useState([]);
+    const [currentUser,setCurrentUser]=useState([])
 
+
+useEffect(()=>{
+  Promise.all([api.getPostList(),api.getUserInfo() ]).then(([dataPosts, dataUser])=>{
+    setCards(dataPosts);
+    console.log(dataPosts);
+    setCurrentUser(dataUser);
+  });
+// api.getPostList().then((data)=>setCards(data.posts));
+// api.getUserInfo().then((dataUser)=>setCurrentUser(dataUser))
+},[])
 
   return (
-    <main className='content_container'>
-    <div className='content_carts'>
+    
+    <div className='content_container'>
+      <div className='content_carts'>
       
-    <CardList  />
-    </div>
-   </main>
+        <CardList goodData={cards}  />  
+      </div>
+   </div>
+   
   );
 }
 
