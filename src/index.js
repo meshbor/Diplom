fe0ea0a5dbf8aedab6c1ede93c943addd163654d
Михаил <91387796+StMihaile@users.noreply.github.com>
2023-02-15 23:26:01 +0400

    Слияние проекта

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from '../src/components/app/App';
+import App from '../src/components/App/app';
 import reportWebVitals from './reportWebVitals';
 import { createRootApp } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
